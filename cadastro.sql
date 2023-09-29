154b2de16c19bc0b3fe295be6423a1cc7adc391f
Raphaela Rodrigues Luvizotto <124827784+raphaelaluvi@users.noreply.github.com>
2023-09-29 17:44:17 -0300

    Delete cadastro.sql

--- cadastro.sql
+++
@@ -1,8 +0,0 @@
-insert into pessoas
-(id, nome, nascimento, genero, peso, altura, nacionalidade)
-values
-(default, 'Viviane', '1976-05-17', 'F', '60', '1.60', 'México'),
-(default, 'Luis', '2009-10-16', 'M', '55', '1.70', 'Espanha'),
-(default, 'Aurora', '2021-11-12', 'F', '10', '0.90', default);
-
-select * from pessoas;
